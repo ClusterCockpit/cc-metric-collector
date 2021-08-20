1cc9491eca53b3823101c9da3f3a0da5618ecd40
Lou Knauer <lou.knauer@gmx.de>
2021-08-20 12:17:46 +0200

    Reset encoder buffer in nats sink

--- sinks/natsSink.go
+++ sinks/natsSink.go
@@ -66,7 +66,7 @@ func (s *NatsSink) Write(measurement string, tags map[string]string, fields map[
 			return err
 		}
 		s.client.Publish(s.database, s.buffer.Bytes())
-
+		s.buffer.Reset()
 	}
 	return nil
 }
