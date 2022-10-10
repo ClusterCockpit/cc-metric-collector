9e746006e8ac2425e48c5957231c0262062ea8ac
Thomas Gruber <Thomas.Roehl@googlemail.com>
2022-10-10 12:12:11 +0200

    Update lustreMetric.go

--- collectors/lustreMetric.go
+++ collectors/lustreMetric.go
@@ -10,8 +10,8 @@ import (
 	"strings"
 	"time"
 
-	cclog "github.com/ClusterCockpit/cc-metric-collector/internal/ccLogger"
-	lp "github.com/ClusterCockpit/cc-metric-collector/internal/ccMetric"
+	cclog "github.com/ClusterCockpit/cc-metric-collector/pkg/ccLogger"
+	lp "github.com/ClusterCockpit/cc-metric-collector/pkg/ccMetric"
 )
 
 type LustreCollectorConfig struct {
