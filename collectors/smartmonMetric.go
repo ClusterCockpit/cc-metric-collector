3d5b28e5aab4653117ec5abf6b057bc0c28c8d35
Thomas Gruber <Thomas.Roehl@googlemail.com>
2022-10-10 12:10:22 +0200

    Update smartmonMetric.go

--- collectors/smartmonMetric.go
+++ collectors/smartmonMetric.go
@@ -5,8 +5,8 @@ import (
 	"os/exec"
 	"time"
 
-	cclog "github.com/ClusterCockpit/cc-metric-collector/internal/ccLogger"
-	lp "github.com/ClusterCockpit/cc-metric-collector/internal/ccMetric"
+	cclog "github.com/ClusterCockpit/cc-metric-collector/pkg/ccLogger"
+	lp "github.com/ClusterCockpit/cc-metric-collector/pkg/ccMetric"
 )
 
 type SmartMonCollectorConfig struct {
