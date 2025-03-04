76e36373f1bb11255f728d37326cb3eaebe84e9a
brinkcoder <Robert.Externbrink@ruhr-uni-bochum.de>
2025-03-05 00:45:45 +0100

    switch to cc-lib

--- collectors/collectorManager.go
+++ collectors/collectorManager.go
@@ -6,7 +6,7 @@ import (
 	"sync"
 	"time"
 
-	lp "github.com/ClusterCockpit/cc-energy-manager/pkg/cc-message"
+	lp "github.com/ClusterCockpit/cc-lib/pkg/ccMessage"
 	cclog "github.com/ClusterCockpit/cc-metric-collector/pkg/ccLogger"
 	mct "github.com/ClusterCockpit/cc-metric-collector/pkg/multiChanTicker"
 )
